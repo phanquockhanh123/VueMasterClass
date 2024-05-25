66d38c6b40a2b6776029fff8a3956e86d2d82d40
phanquockhanh123 <khanhphanquoc68@gmail.com>
2024-05-25 21:49:01 +0700

    init project vuemaster class 3

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <h1 class="mt-8 text-center text-6xl">Hello Vue</h1>
+  <MainNav></MainNav>
+</template>
+
+<script>
+import MainNav from "@/components/MainNav.vue";
+
+export default {
+  components: {
+    MainNav
+  }
+}
+</script>
