0049e926fbdfaac97861f45b5976bdabf01726be
PHAN QUOC KHANH <khanhphanquoc68@lgcns.com>
2024-05-27 02:54:31 -0700

    vue image in grid

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -9,9 +9,10 @@
           <head-line></head-line>
           <job-search-form />
         </div>
-        <div class="col-span-5 col-start-7">
-          
+        <div class="col-span-5 col-start-7 self-center justify-self-center">
+          <img class="h-80 w-80 object-contain" src="https://img.freepik.com/free-vector/male-cartoon-face-closing-his-eyes_1308-135183.jpg?t=st=1716776502~exp=1716777102~hmac=b1ac7f0c492ea4b0d3675cd58f5" />
         </div>
+        
         <div class="col-span-1 col-start-12">
           
         </div>
